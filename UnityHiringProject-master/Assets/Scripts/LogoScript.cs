f6012fe072c0fc4cdc0d3fbdbf7680d63b394b42
Dominykas Osipovičius <domasvz1@gmail.com>
2019-11-02 17:23:48 +0200

    Remaking my 2d cars game into space invaders
    Adding master branch, also changing menu adding sounds and changing backgrounds.

---
+++ UnityHiringProject-master/Assets/Scripts/LogoScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoScript : MonoBehaviour {
+
+    float scale = 2.3f;
+    readonly WaitForSeconds waitForSeconds = new WaitForSeconds(0.6f);
+
+    
+    IEnumerator Start()
+    { 
+        // The second we start the menu script we start the menu music
+        GameObject.FindGameObjectWithTag("Music").GetComponent<MusicScript>().PlayMusic();
+
+        // I'm using LogoScript in Menu and HowToPlay Scenes, so I play the continue playing music in both of them
+        while (true)
+        {
+            if (scale == 2.3f)
+                scale = 1.7f;
+            else if (scale == 1.7f)
+                scale = 2.3f;
+            transform.localScale = new Vector2(scale, scale);
+
+            yield return waitForSeconds;
+        }
+    }
+}
---
+++ UnityHiringProject-master/Assets/UpdateMenuItems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMenuItems : MonoBehaviour {
+
+    // Add this to list and later laucn through loop
+    public new Camera camera;
+    public Transform transLogo;
+    public Transform transintroText;
+    public Transform transPlay;
+    public Transform transHowTo;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // During the update we get transform component and set its position in the middle of the screen
+        Transform trans = GetComponent<Transform>();
+        transLogo.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y + 18, camera.transform.position.z + 6);
+        transintroText.transform.position = new Vector3(camera.transform.position.x + 3, camera.transform.position.y - 8, camera.transform.position.z + 6);
+        // Last two objects oin the menu
+        transPlay.transform.position = new Vector3(camera.transform.position.x - 10, camera.transform.position.y - 20, camera.transform.position.z + 6);
+        transHowTo.transform.position = new Vector3(camera.transform.position.x + 10, camera.transform.position.y - 20, camera.transform.position.z + 6);
+    }
+}
