b199bee378d36806894b14a2e8fdb07178541f22
Dominykas Osipovičius <domasvz1@gmail.com>
2019-11-03 16:13:56 +0200

    tweak to the menu

--- Assets/Scripts/LogoScript.cs
+++ Assets/Scripts/LogoScript.cs
@@ -3,25 +3,31 @@ using System.Collections;
 
 public class LogoScript : MonoBehaviour {
 
-    float scale = 2.3f;
-    readonly WaitForSeconds waitForSeconds = new WaitForSeconds(0.6f);
+    private float scale = 2.3f;
 
-    
-    IEnumerator Start()
+    public WaitForSeconds WaitForSeconds
+    {
+        get
+        {
+            return new WaitForSeconds(0.6f);
+        }
+    }
+
+    private IEnumerator Start()
     { 
         // The second we start the menu script we start the menu music
         GameObject.FindGameObjectWithTag("Music").GetComponent<MusicScript>().PlayMusic();
-
-        // I'm using LogoScript in Menu and HowToPlay Scenes, so I play the continue playing music in both of them
+        
+        // I'm using LogoScript in Menu and Instruction Scenes, so I play the continue playing music in both of them
         while (true)
         {
             if (scale == 2.3f)
                 scale = 1.7f;
             else if (scale == 1.7f)
                 scale = 2.3f;
-            transform.localScale = new Vector2(scale, scale);
+            transform.localScale = new Vector3(scale, scale, scale);
 
-            yield return waitForSeconds;
+            yield return WaitForSeconds;
         }
     }
 }
