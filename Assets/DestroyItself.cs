74f10115cf9efbc59333992ccbad8125bf3bdbc4
Dominykas Osipovičius <domasvz1@gmail.com>
2019-11-06 17:10:43 +0200

    Created Explosion Prefab

---
+++ Assets/DestroyItself.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItself : MonoBehaviour {
+
+    void Start()
+    {
+        Destroy(gameObject, 2.0f);
+    }
+}
+	
+
