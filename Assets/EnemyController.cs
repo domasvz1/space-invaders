7b3a557a9c9f13931d65f0248a2a895daceac026
Dominykas Osipovičius <domasvz1@gmail.com>
2019-11-06 17:42:49 +0200

    Made Enemy Prefab with engine and shooting sound, all pretty much the same as the player

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    // This can be from gameController
+    public float shootDelay = 2.0f;
+    public float repeatRate = 2.0f;
+
+    // this needs to be assigned in inspector
+    public GameObject Bullet;
+    public Transform BulletSpawn;
+
+	// Use this for initialization
+	void Start () {
+        // This can be called from th GameController
+        InvokeRepeating("Shoot", shootDelay, repeatRate);
+	}
+	
+	public void Shoot()
+    {
+        Instantiate(Bullet, BulletSpawn.position, BulletSpawn.rotation);
+        GetComponent<AudioSource>().Play();
+    }
+
+}
